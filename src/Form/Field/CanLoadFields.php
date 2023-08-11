bcc149c0ce6ce42ca0919407a6b3c464f7bbf86c
Jason <chenjxlg@163.com>
2023-08-11 08:59:11 +0800

    联动表单，可选作用域

--- src/Form/Field/CanLoadFields.php
+++ src/Form/Field/CanLoadFields.php
@@ -13,24 +13,36 @@ trait CanLoadFields
      * @param  string  $sourceUrl
      * @param  string  $idField
      * @param  string  $textField
+     * @param  string  $group
      * @return $this
      */
-    public function load($field, $sourceUrl, string $idField = 'id', string $textField = 'text')
-    {
-        return $this->loads($field, $sourceUrl, $idField, $textField);
+    public function load(
+        $field,
+        $sourceUrl,
+        string $idField = 'id',
+        string $textField = 'text',
+        string $group = '.fields-group'
+    ): static {
+        return $this->loads($field, $sourceUrl, $idField, $textField, $group);
     }
 
     /**
      * 联动加载多个字段.
      *
-     * @param  array|string  $fields
-     * @param  array|string  $sourceUrls
+     * @param  array  $fields
+     * @param  array  $sourceUrls
      * @param  string  $idField
      * @param  string  $textField
+     * @param  string  $group
      * @return $this
      */
-    public function loads($fields = [], $sourceUrls = [], string $idField = 'id', string $textField = 'text')
-    {
+    public function loads(
+        $fields = [],
+        $sourceUrls = [],
+        string $idField = 'id',
+        string $textField = 'text',
+        string $group = '.fields-group'
+    ): static {
         $fieldsStr = implode('^', array_map(function ($field) {
             if (Str::contains($field, '.')) {
                 return $this->normalizeElementClass($field).'_';
@@ -38,15 +50,18 @@ trait CanLoadFields
 
             return $this->normalizeElementClass($field);
         }, (array) $fields));
-        $urlsStr = implode('^', array_map(function ($url) {
+        $urlsStr   = implode('^', array_map(function ($url) {
             return admin_url($url);
         }, (array) $sourceUrls));
 
-        return $this->addVariables(['loads' => [
-            'fields'    => $fieldsStr,
-            'urls'      => $urlsStr,
-            'idField'   => $idField,
-            'textField' => $textField,
-        ]]);
+        return $this->addVariables([
+            'loads' => [
+                'group'     => $group,
+                'fields'    => $fieldsStr,
+                'urls'      => $urlsStr,
+                'idField'   => $idField,
+                'textField' => $textField,
+            ],
+        ]);
     }
 }
